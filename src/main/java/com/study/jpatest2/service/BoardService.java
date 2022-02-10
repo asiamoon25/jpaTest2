3677cb147d396bf99fcbfadb6cdce4efcdb58c43
LENOVO <wjddustkd45@gmail.com>
2022-02-10 16:37:18 +0900

    2022/02/10 commit

--- src/main/java/com/study/jpatest2/service/BoardService.java
+++ src/main/java/com/study/jpatest2/service/BoardService.java
@@ -2,18 +2,24 @@ package com.study.jpatest2.service;
 
 import com.study.jpatest2.repository.BoardRepository;
 import com.study.jpatest2.vo.BoardVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 public class BoardService {
     private final BoardRepository boardRepository;
+
 
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
-
+    @Transactional(readOnly = true)
+    @Cacheable(value = "boardList")
     public Page<BoardVO> findAll(Pageable pageable){
 
         return boardRepository.findAll(pageable);
